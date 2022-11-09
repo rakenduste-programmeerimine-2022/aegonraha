1b16d7334c64d66ad5c07daec00652f001570247
Citronnelle <taikiviia@gmail.com>
2022-11-09 18:00:12 +0200

    Added folder structure & app skeleton (WF, FE w/ CRA, BE)

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import "./App.css"
+import React from "react"
+import { Box } from "@mui/material/"
+
+const App = () => {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+        gap: "25px",
+        height: "100vh"
+      }}
+    >
+      👀
+    </Box>
+  )
+}
+
+export default App
